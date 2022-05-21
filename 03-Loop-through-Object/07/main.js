f623c0e883d37c5cc5021245b110e8e0f4cf4517
Supattana-S <supattana.sue@gmail.com>
2022-05-21 22:05:00 +0700

    Done lab3.7

---
+++ 03-Loop-through-Object/07/main.js
@@ -0,0 +1,35 @@
+const employees = {
+  john: {
+    salary: 1000,
+    address: { district: "Ratchathewi", province: "Bangkok" },
+  },
+  peter: {
+    salary: 1500,
+    address: { district: "Pathumwan", province: "Bangkok" },
+  },
+  mike: {
+    salary: 800,
+    address: { district: "Pakkret", province: "Nonthaburi" },
+  },
+  sarah: {
+    salary: 2200,
+    address: { district: "Sriraja", province: "Chonburi" },
+  },
+};
+
+function checkName(name, obj) {
+  for (let k in obj) {
+    if (k === name) {
+      return true;
+    }
+    return false;
+  }
+}
+
+let inputName = prompt("Please input your name");
+
+if (checkName(inputName, employees)) {
+  console.log(
+    `Name: ${inputName}, salary: ${employees[inputName].salary}, Address: ${employees[inputName].address.district}, ${employees[inputName].address.province}`
+  );
+}
