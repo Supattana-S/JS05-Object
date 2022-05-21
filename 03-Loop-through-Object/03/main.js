0809c4dd16bd5d59c1b9725d8557aeb5b5b6d537
Supattana-S <supattana.sue@gmail.com>
2022-05-21 20:37:48 +0700

    done lab3.3

---
+++ 03-Loop-through-Object/03/main.js
@@ -0,0 +1,11 @@
+const user = {};
+
+let username = prompt("Enter username");
+let password = prompt('Enter password')
+let email = prompt('Enter E-mail')
+
+user.username = username
+user.password = password
+user.email = email
+
+console.log(user)
