751f7d2e93a5c673708f148fefa30fef132f9fe9
Supattana-S <supattana.sue@gmail.com>
2022-05-21 21:05:09 +0700

    Done lab 3.4

---
+++ 03-Loop-through-Object/04/main.js
@@ -0,0 +1,23 @@
+const a = {};
+let b = true;
+let key;
+let value;
+
+function checkToStop(sth) {
+  return sth === "stop" || sth === null || String(sth.trim()) === "";
+}
+
+while (true) {
+  key = prompt("Please input your key");
+  if (checkToStop(key)) {
+    break;
+  }
+  value = prompt("Please input your value");
+  if (checkToStop(value)) {
+    break;
+  }
+
+  a[key] = value;
+}
+
+console.log(a);
